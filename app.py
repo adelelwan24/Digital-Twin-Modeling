a0033d5be9c5898ad3386f032cedb98c165a8ea9
adelelwan24 <adelelwan24@gmail.com>
2024-07-09 18:54:58 +0200

    Add editor and viewer endpoints

--- app.py
+++ app.py
@@ -1,59 +1,56 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, send_from_directory
 import os
 from werkzeug.utils import secure_filename
+from main import FloorplanToBlenderRunner 
 
-from main import FloorplanToBlenderRunner
-import os
+app = Flask(__name__, static_url_path='')
 
-
-app = Flask(__name__)
-
-# Configure upload folder and allowed extensions
-UPLOAD_FOLDER = 'uploads/'
+UPLOAD_FOLDER = 'static/uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-
-# Ensure the upload folder exists
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-
 def allowed_file(filename):
-    """Check if the file has one of the allowed extensions."""
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # Check if the post request has the file part
-        if 'file' not in request.files:
-            return redirect(request.url)
         file = request.files['file']
-
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            return redirect(request.url)
-        
-        if file and allowed_file(file.filename):
+        if file and file.filename and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            
-            print('filename: ', filename)
-
             image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(image_path)
-            FloorplanToBlenderRunner(image_path)
-            return f"File uploaded successfully: {filename}"
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new Image</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+            
+            name = filename.split('.')[0]
+            if os.path.exists(f"static/models/floorplan/{name}.glb"):
+                model_name = f"{name}.glb"
+                return render_template('viewer.html', model_name=model_name)
+                
+            floor_path = FloorplanToBlenderRunner(image_path, name)
+            model_name = os.path.split(floor_path)[-1]
+            print(f'Floor Path: {floor_path} | File Name: {model_name}')
+            return render_template('viewer.html', model_name=model_name)
+
+        else:
+            return redirect(request.url)
+    return render_template('upload.html')
+
+@app.route('/editor')
+def display():
+    # return render_template('3d.html', model='final_object.glb')
+    # return render_template('drag_cubes.html')
+    # return render_template('drag.html')
+    return render_template('editor.html')
+
+@app.route('/viewer')
+def viewer():
+    return render_template('viewer.html', model='final_object.glb')
+    # return render_template('drag.html')
+
+@app.route('/floor')
+def send_report():
+    return send_from_directory('static', 'models/floorplan.glb')
 
 if __name__ == '__main__':
     app.run(debug=True)
