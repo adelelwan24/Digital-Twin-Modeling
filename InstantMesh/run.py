4fa8c1f334a7e6ec61449b895db65163c7aa5248
adelelwan24 <adelelwan24@gmail.com>
2024-07-17 00:52:16 +0200

    Include scaling for imported objects & Allow deletion of selected object

--- InstantMesh/run.py
+++ InstantMesh/run.py
@@ -238,14 +238,14 @@ for idx, sample in enumerate(outputs):
                 tex_map.permute(1, 2, 0).data.cpu().numpy(),
                 mesh_path_idx,
             )
+            print(f"Mesh saved to {mesh_path_idx}")
         else:
             vertices, faces, vertex_colors = mesh_out
             # save_obj(vertices, faces, vertex_colors, mesh_path_idx)
             
             #### FIXED: Changed the output model format to GLB for compatability with the front-end
             save_glb(vertices, faces, vertex_colors, mesh_path_idx_glb)
-
-        print(f"Mesh saved to {mesh_path_idx}")
+            print(f"Mesh saved to {mesh_path_idx_glb}")
 
         # get video
         if args.save_video:
